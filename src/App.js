cff948a46d17f9897fa07ab2afc7de778ea48e73
yunbo <cscim918@gmail.com>
2021-01-21 23:06:43 +0900

    2021/1/21

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import Movie from './components/Movie';
+import MovieForm from './components/MovieForm';
 
 function App() {
-  const movies = [
-    { title: 'yunbo1', year: 1996 },
-    { title: 'yunbo2', year: 1997 },
-    { title: 'yunbo3', year: 1998 },
-    { title: 'yunbo4', year: 1999 },
-  ];
-  const renderMovies = movies.map((movie) => {
-    return <Movie movie={movie} key={movie.title} />;
-  });
+  const [movies, setMovies] = useState([]);
+
+  const removeMovie = (id) => {
+    setMovies(
+      movies.filter((movie) => {
+        return movie.id !== id;
+      })
+    );
+  };
+  const renderMovies = movies.length
+    ? movies.map((movie) => {
+        return <Movie movie={movie} key={movie.id} removeMovie={removeMovie} />;
+      })
+    : '추가된 영화가 없습니다.';
+  const addMovie = (movie) => {
+    setMovies([...movies, movie]);
+  };
   return (
     <div className="App">
       <h1>Movie list</h1>
+      <MovieForm addMovie={addMovie} />
       {renderMovies}
     </div>
   );
